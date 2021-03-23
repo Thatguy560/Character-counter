5e1857a4cffd8b77a56d5c8798b7168bce7d5695
Peter Stevens <peterstevens510@gmail.com>
2021-03-23 17:38:48 +0000

    To add function will be responsible for the word counting

--- src/App.js
+++ src/App.js
@@ -6,38 +6,46 @@ class App extends Component {
     super(props);
     this.state = {
       firstValue: "",
-      characterCount: "",
+      numberOfCharacters: "",
       withoutWhiteSpace: "",
-      wordCount: "",
+      numberOfWords: "",
       linesCount: "",
       wordSelectionCount: "",
     };
   }
 
   firstHandle = (event) => {
-    const input = event.target.value;
-    console.log(input);
+    var input = event.target.value;
+
     const text = document.getElementById("textarea").value;
     const linesCount = text.split("/\r|\r\n|\n/").length;
-    const characterCount = input === "" ? 0 : input.split("").length;
+    const numberOfCharacters = input === "" ? 0 : input.split("").length;
+
     const withoutWhiteSpace =
       input === "" ? 0 : input.split("").filter((char) => char !== " ").length;
     const words =
       input === "" ? 0 : input.split(" ").filter((word) => word.trim()).length;
     const lines = input === "" ? 1 : input.split(/\n/g).length;
+
     this.setState({
       firstValue: input,
-      characterCount: characterCount,
+      numberOfCharacters: numberOfCharacters,
       withoutWhiteSpace: withoutWhiteSpace,
-      wordCount: words,
+      numberOfWords: words,
       linesCount: lines,
     });
   };
 
+  // This function will be responsible for the word counting
+  test = (e) => {
+    console.log(this.state.firstValue);
+    e.preventDefault();
+  };
+
   render() {
-    var characterCount = this.state.characterCount;
+    var numberOfCharacters = this.state.numberOfCharacters;
     var withoutWhiteSpace = this.state.withoutWhiteSpace;
-    var words = this.state.wordCount;
+    var words = this.state.numberOfWords;
     var lines = this.state.linesCount;
     return (
       <div className="App">
@@ -45,10 +53,9 @@ class App extends Component {
           <form>
             <h1>Character Counter</h1>
             <p>
-              Characters <span>{characterCount}</span> Without White Space{" "}
-              <span>{withoutWhiteSpace}</span> Words <span>{words}</span> Lines
-              {"  "}
-              <span>{lines}</span>
+              Characters <span>{numberOfCharacters}</span> Without White Space{" "}
+              <span>{withoutWhiteSpace}</span> Words <span>{words}</span> New
+              Lines <span>{lines}</span>
             </p>
             <textarea
               id="textarea"
@@ -57,7 +64,13 @@ class App extends Component {
               value={this.firstValue}
               onChange={this.firstHandle}
             />
-            {/* <p>test</p>  */}
+            <h1>Word Counting</h1>
+            <button className="btn" onClick={this.test}>
+              test
+            </button>
+            <p>
+              <span>Word</span> <span>Count</span>{" "}
+            </p>
             {/* Have a feature which displays certain words and counts them for much they're used */}
           </form>
         </header>
@@ -67,3 +80,10 @@ class App extends Component {
 }
 
 export default App;
+
+// var h0 = document.getElementById("textarea").style.height;
+// document.getElementById("textarea").style.height = "auto";
+// var h1 = document.getElementById("textarea").scrollHeight;
+// document.getElementById("textarea").style.height = h0;
+// var lineHeight = 20;
+// console.log(Math.ceil(h1 / lineHeight));
